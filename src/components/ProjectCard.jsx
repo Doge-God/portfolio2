1e539ccddce800ed31e30997970125af098ca786
Fu Zhou <Futianzhou@hotmail.com>
2023-08-10 16:39:10 +1200

    Feat: project cards & highlight

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,26 @@
+
+import Tilt from "react-parallax-tilt"
+
+function ProjectCard(props) {
+
+  return (
+        <Tilt glareEnable={true} glareMaxOpacity={0.3} glareColor="#e8dcfc" glarePosition="bottom" glareBorderRadius="5px" perspective={1500}>
+          <div className=" inner-element w-full h-fit pt-2 pb-10 px-2 mb-8
+              shadow-lg-neu-5 rounded-2xl border-t-2 border-l-2 border-white
+              flex flex-col break-inside-avoid"
+              onClick={()=>window.open(props.extLink, "_blank")}>
+            <img src={props.imgSrc} className=' w-full h-64 object-cover rounded-xl shadow-inner shadow-black border-white border-r-2 border-l-2'></img>
+            <div className=' mx-2 my-2 text-xl font-plex font-medium text-ui-key'>{props.title}</div>
+            <div className=' text-xs mx-2 font-plex text-ui-main'>
+                {props.description}
+            </div>
+        </div>
+        </Tilt>
+        
+        
+        
+  );
+}
+//onClick={()=>window.open(prop.extLink, "_blank")}
+
+export default ProjectCard;
--- src/pages/HighlightsPage.tsx
+++ src/pages/HighlightsPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
 import routeVariant from "../util/AnimationVariants";
-import ProjectCard from "../components/ProjectCard";
+import ProjectCard from "../components/ProjectCard.jsx";
 
 const DigiExpImg = require("../resource/projectsCard/DigitalExperiments.png")
 const DogeBotImg = require("../resource/projectsCard/DogeBot2.png")
+const PorfolioImg = require("../resource/projectsCard/portfolio.png")
+const KeepfreshImg = require("../resource/projectsCard/Keepfresh.png")
 
 function HighlightsPage () {
     return(
@@ -14,9 +16,13 @@ function HighlightsPage () {
         animate="animate"
         exit="exit">
             <div className="max-w-2xl mx-10 w-full">
-                <div className=" mt-20 mb-3 text-ui-main font-plex text-3xl font-light self-start"> Highlights </div>
+                <div className=" mt-20 text-ui-key font-plex text-3xl font-light self-start"> Highlights </div>
+                <div className=" mb-10 text-ui-main font-plex text-md font-light self-start"> Click to learn more.</div>
                 <div className=" columns-2 gap-8">
-                    <ProjectCard title="Digital Experiments" imgSrc={DogeBotImg} description=" jfdslkajf kjsdah fjksdhjkc hdskja hcjksdh jkc jkchsjk ahcjsd cjksadhc jksdh cjksdhc jkads" extLink={""}></ProjectCard>
+                    <ProjectCard title="Portfolio" imgSrc={PorfolioImg} description="React frontend project. Built with React, TailwindCSS and Typescript." extLink={"https://github.com/Doge-God/portfolio2"}></ProjectCard>
+                    <ProjectCard title="Stack" imgSrc={DogeBotImg} description="A UI/UX time management app prototype I made with Figma. It all stated with a vague client goal of 'reducing stress in life'." extLink={"https://www.behance.net/gallery/152638193/Stack-Time-Management-Game-Prototype"}></ProjectCard>
+                    <ProjectCard title="Digital Experiments" imgSrc={DigiExpImg} description="Three projects in three weeks, where I derive and develop upon some very vague prompts: an phone app to promote public transport targeted at 6 year olds; a poster with your childhood toy; a virtual experience to immerse the user in a different culture." extLink={"https://miro.com/app/board/uXjVPjMcna4=/?share_link_id=123820344246"}></ProjectCard>
+                    <ProjectCard title="Freshkeep" imgSrc={KeepfreshImg} description="An IoT product that turns any fridge into a smart fridge. In this group project I acted as the tech lead." extLink={"https://miro.com/app/board/uXjVOjkub3s=/"}></ProjectCard>
                 </div>
             </div>
             
