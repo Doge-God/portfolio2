8ff13fd4c5113fd4977c431a8dcf2d535d367fa6
FZ <62817503+Doge-God@users.noreply.github.com>
2023-10-02 01:45:01 +1300

    Fix: routing with Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavMenu from './components/NavMenu';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, NavLink, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import HighlightsPage from './pages/HighlightsPage';
 import { AnimatePresence } from 'framer-motion';
@@ -9,12 +9,12 @@ import RoutesWithAnimation from './components/RoutesWithAnimation';
 function App() {
   return (
     <div className="App bg-gradient-to-r from-[#99a3f5] to-[#f4cffa]">
-      <BrowserRouter basename={process.env.REACT_APP_URI}>
+      <HashRouter>
 
           <NavMenu/>
           <RoutesWithAnimation/>
 
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
